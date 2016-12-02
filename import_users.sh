a4a598e7a69c483109b63161c6e11ec9f83abf97
Michael Wittig <post@michaelwittig.info>
2016-12-02 09:27:10 +0100

    fixes #8

--- import_users.sh
+++ import_users.sh
@@ -4,7 +4,9 @@ aws iam list-users --query "Users[].[UserName]" --output text | while read User;
   if id -u "$User" >/dev/null 2>&1; then
     echo "$User exists"
   else
+    #sudo will read each file in /etc/sudoers.d, skipping file names that end in ‘~’ or contain a ‘.’ character to avoid causing problems with package manager or editor temporary/backup files.
+    SaveUserFileName=$(echo "$User" | tr "." " ")
     /usr/sbin/adduser "$User"
-    echo "$User ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/$User"
+    echo "$User ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/$SaveUserFileName"
   fi
 done
