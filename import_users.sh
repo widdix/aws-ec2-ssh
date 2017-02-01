d1f0c036d6f4778fa166dda5b35df2629122d335
shine <hello@shinenelson.com>
2017-02-01 17:53:20 +0530

    Remove logging since the script is cron-automated and would generate un-necessary logs

--- import_users.sh
+++ import_users.sh
@@ -6,9 +6,7 @@ aws iam list-users --query "Users[].[UserName]" --output text | while read User;
   SaveUserName=${SaveUserName//"="/".equal."}
   SaveUserName=${SaveUserName//","/".comma."}
   SaveUserName=${SaveUserName//"@"/".at."}
-  if id -u "$SaveUserName" >/dev/null 2>&1; then
-    echo "$SaveUserName exists"
-  else
+  if ! id -u "$SaveUserName" >/dev/null 2>&1; then
     #sudo will read each file in /etc/sudoers.d, skipping file names that end in ‘~’ or contain a ‘.’ character to avoid causing problems with package manager or editor temporary/backup files.
     SaveUserFileName=$(echo "$SaveUserName" | tr "." " ")
     /usr/sbin/adduser "$SaveUserName"
