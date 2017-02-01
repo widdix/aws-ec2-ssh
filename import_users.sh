16ab31b40a92a323dce4e1deaec5516aee9739c8
shine <hello@shinenelson.com>
2017-02-01 16:39:01 +0530

    Changing adduser command to useradd since adduser is interactive on debian-based systems. (Resolves Issue #10)

--- import_users.sh
+++ import_users.sh
@@ -11,7 +11,7 @@ aws iam list-users --query "Users[].[UserName]" --output text | while read User;
   else
     #sudo will read each file in /etc/sudoers.d, skipping file names that end in ‘~’ or contain a ‘.’ character to avoid causing problems with package manager or editor temporary/backup files.
     SaveUserFileName=$(echo "$SaveUserName" | tr "." " ")
-    /usr/sbin/adduser "$SaveUserName"
+    /usr/sbin/useradd "$SaveUserName"
     echo "$SaveUserName ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/$SaveUserFileName"
   fi
 done
