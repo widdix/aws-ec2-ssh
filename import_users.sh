a0a6712d3dd76006a092fb879d0758b7d1552cad
shine <hello@shinenelson.com>
2017-02-02 21:22:48 +0530

    add useradd flags --create-home, --shell (#16)
    * add useradd flags --create-home, --shell (Resolves comment #276948082)
    
    * Re-arraging order of flags passed to useradd (Resolves comment #276986409)

--- import_users.sh
+++ import_users.sh
@@ -8,7 +8,7 @@ aws iam list-users --query "Users[].[UserName]" --output text | while read User;
   SaveUserName=${SaveUserName//"@"/".at."}
   if ! id -u "$SaveUserName" >/dev/null 2>&1; then
     # sudo will read each file in /etc/sudoers.d, skipping file names that end in ‘~’ or contain a ‘.’ character to avoid causing problems with package manager or editor temporary/backup files.
-    /usr/sbin/useradd "$SaveUserName"
+    /usr/sbin/useradd --create-home --shell /bin/bash "$SaveUserName" 
     # Uncomment the following lines if you need to give all users sudo privileges
     # SaveUserFileName=$(echo "$SaveUserName" | tr "." " ")
     # echo "$SaveUserName ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/$SaveUserFileName"
