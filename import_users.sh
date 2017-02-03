08ea3c696adde236b00437e2bd3a94f0f607627c
shine <hello@shinenelson.com>
2017-02-03 13:18:04 +0530

    Hack to check the only first column of /etc/passwd delimited by ':' for usernames inorder to avoid skipping of usernames that are subsets of other usernames or users with same home directories. (Resolves comment #277179303)

--- import_users.sh
+++ import_users.sh
@@ -6,7 +6,7 @@ aws iam list-users --query "Users[].[UserName]" --output text | while read User;
   SaveUserName=${SaveUserName//"="/".equal."}
   SaveUserName=${SaveUserName//","/".comma."}
   SaveUserName=${SaveUserName//"@"/".at."}
-  if ! grep "$SaveUserName" /etc/passwd > /dev/null; then
+  if ! grep "^$SaveUserName:" /etc/passwd > /dev/null; then
     # sudo will read each file in /etc/sudoers.d, skipping file names that end in ‘~’ or contain a ‘.’ character to avoid causing problems with package manager or editor temporary/backup files.
     /usr/sbin/useradd --create-home --shell /bin/bash "$SaveUserName" 
     # Uncomment the following lines if you need to give all users sudo privileges
