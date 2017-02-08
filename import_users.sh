17313a627073f2466d926af8d8bdac9e35e088a8
Luke Goodsell <l.goodsell@achillestx.com>
2017-02-08 09:56:20 +0000

    Moved SaveUserFileName to more sensible location

--- import_users.sh
+++ import_users.sh
@@ -14,16 +14,16 @@ aws iam list-users --query "Users[].[UserName]" --output text | while read User;
   SaveUserName=${SaveUserName//"="/".equal."}
   SaveUserName=${SaveUserName//","/".comma."}
   SaveUserName=${SaveUserName//"@"/".at."}
-  # sudo will read each file in /etc/sudoers.d, skipping file names that end in
-  # ‘~’ or contain a ‘.’ character to avoid causing problems with package
-  # manager or editor temporary/backup files.
-  SaveUserFileName=$(echo "$SaveUserName" | tr "." " ")
-  SaveUserSudoFilePath="/etc/sudoers.d/$SaveUserFileName"
   if ! grep "^$SaveUserName:" /etc/passwd > /dev/null; then
     /usr/sbin/useradd --create-home --shell /bin/bash "$SaveUserName"
   fi
 
   if [[ ! -z "${SudoersGroup}" ]]; then
+    # sudo will read each file in /etc/sudoers.d, skipping file names that end
+    # in ‘~’ or contain a ‘.’ character to avoid causing problems with package
+    # manager or editor temporary/backup files.
+    SaveUserFileName=$(echo "$SaveUserName" | tr "." " ")
+    SaveUserSudoFilePath="/etc/sudoers.d/$SaveUserFileName"
     if [[ "${SudoersGroup}" == "##ALL##" ]] || echo "$Sudoers" | grep "^$User\$" > /dev/null; then
       echo "$SaveUserName ALL=(ALL) NOPASSWD:ALL" > "$SaveUserSudoFilePath"
     else
