ebfe57c27de92aa1124dfbc178168d05acb2c94d
Luke Goodsell <l.goodsell@achillestx.com>
2017-02-08 10:28:21 +0000

    Added documentation to README, additional policy permission, and note in install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,12 @@ cd $tmpdir/aws-ec2-ssh
 cp authorized_keys_command.sh /opt/authorized_keys_command.sh
 cp import_users.sh /opt/import_users.sh
 
+# To control which users are given sudo privileges, uncomment the line below
+# changing GROUPNAME to either the name of the IAM group for sudo users, or
+# to ##ALL## to give all users sudo access. If you leave it blank, no users will
+# be given sudo access.
+#sudo sed -i 's/SudoersGroup=""/SudoersGroup="GROUPNAME"/' /opt/import_users.sh
+
 sed -i 's:#AuthorizedKeysCommand none:AuthorizedKeysCommand /opt/authorized_keys_command.sh:g' /etc/ssh/sshd_config
 sed -i 's:#AuthorizedKeysCommandUser nobody:AuthorizedKeysCommandUser nobody:g' /etc/ssh/sshd_config
 
