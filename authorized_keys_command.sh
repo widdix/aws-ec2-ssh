4f89a1b353a5cc85f6b1d43b247d9827da4ab8f2
shine <hello@shinenelson.com>
2017-02-25 01:29:51 +0530

    Authorizing users based on AWS IAM User Groups (Resolves comment #281132929)

--- authorized_keys_command.sh
+++ authorized_keys_command.sh
@@ -10,6 +10,26 @@ SaveUserName=${SaveUserName//"="/".equal."}
 SaveUserName=${SaveUserName//","/".comma."}
 SaveUserName=${SaveUserName//"@"/".at."}
 
-aws iam list-ssh-public-keys --user-name "$SaveUserName" --query "SSHPublicKeys[?Status == 'Active'].[SSHPublicKeyId]" --output text | while read KeyId; do
-  aws iam get-ssh-public-key --user-name "$SaveUserName" --ssh-public-key-id "$KeyId" --encoding SSH --query "SSHPublicKey.SSHPublicKeyBody" --output text
+# Specify IAM group(s) separated by spaces to import users.
+# Specify "##ALL##" (including the double quotes) to import all users
+IAMGroup=("##ALL##")
+
+[ -z "$IAMGroup" ] && IAMGroup="##ALL##"          # Check for empty ImportGroup
+
+for group in ${IAMGroup[@]}; do
+  # Generalizing Query String to avoid multiple conditional loops
+  if [ -n "${IAMGroup}" ] && [ "${IAMGroup}" != "##ALL##" ]; then
+    queryStr="get-group --group-name $group"
+  else
+    queryStr="list-users"
+  fi
+
+  for user in $( aws iam $queryStr --query "Users[].[UserName]" --output text ); do
+    if [ $user == $SaveUserName ]; then
+      for KeyId in $( aws iam list-ssh-public-keys --user-name "$SaveUserName" --query "SSHPublicKeys[?Status == 'Active'].[SSHPublicKeyId]" --output text ); do
+        aws iam get-ssh-public-key --user-name "$SaveUserName" --ssh-public-key-id "$KeyId" --encoding SSH --query "SSHPublicKey.SSHPublicKeyBody" --output text
+      done
+      break
+    fi
+  done
 done
--- install.sh
+++ install.sh
@@ -13,6 +13,11 @@ cd $tmpdir/aws-ec2-ssh
 cp authorized_keys_command.sh /opt/authorized_keys_command.sh
 cp import_users.sh /opt/import_users.sh
 
+# Specify IAM group(s) separated by spaces to import users.
+# Replace <IAMGroups> with the groups to import separated by spaces
+# Specify "##ALL##" (including the double quotes) to import all users
+# sudo sed -i 's/IAMGroup=("##ALL##")/IAMGroup=(<IAMGroups>)/' /opt/authorized_keys_command.sh
+
 # To control which users are given sudo privileges, uncomment the line below
 # changing GROUPNAME to either the name of the IAM group for sudo users, or
 # to ##ALL## to give all users sudo access. If you leave it blank, no users will
