0d8260f3a7e7e2301e667606ca51d04790c0e423
Michiel van Baak <michiel@vanbaak.info>
2017-02-26 13:03:12 +0100

    Handle iam users with a dash in the name

--- import_users.sh
+++ import_users.sh
@@ -106,6 +106,7 @@ function clean_iam_username() {
     clean_username=${clean_username//"="/".equal."}
     clean_username=${clean_username//","/".comma."}
     clean_username=${clean_username//"@"/".at."}
+    clean_username=${clean_username//"-"/".dash."}
     echo "${clean_username}"
 }
 
