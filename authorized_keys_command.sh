5831d028766463e3d6e57600d2d694784e4f9b4b
Michiel van Baak <michiel@vanbaak.eu>
2017-04-04 14:02:05 +0200

    Merge of my AssumeRole branch, and some other forks (#24)
    * If defined, use IAM AssumeRole to fetch users and ssh keys from another AWS account
    
    * Untested: Fold the ability to use IAM AssumeRole in the showcase CF template
    
    * Import only specified IAM groups
    
    While trying to implement this feature looking at the many forks,
    I decided to mix the various implementations into something for
    ourselves.
    
    Split up the script in functions
    Created some meaningfull global variables
    
    * Change variable name so its the same as in the import_users.sh. Use read -r
    
    * Restore ability to import all users by leaving IAM_AUTHORIZED_GROUPS empty
    
    * Restore ability to specify an IAM group that should be added to sudo
    
    * Add assumerole to install.sh
    
    * Document how to setup cross account access
    
    * Use same markdown style as the README.md
    
    * Default to import all IAM users and update install.sh to explain how to limit the IAM groups to import
    
    * Make the LOCAL_GROUPS optional and document in install.sh
    
    * Handle iam users with a dash in the name
    
    * Remove users no longer in the IAM groups we give access to the instance
    
    * Handle the situation where a user is in more then one IAM group we want to sync
    
    * use full path to usermod binary
    
    * some more absolute paths to binaries that are normally not in the $PATH of cron
    
    * And use absolute path to groupadd as well since cron could not find it
    
    * fix typo
    
    * Allow usernames with a dash. Thanks for spotting this one @malytic
    
    * When checking the ssh key, make sure we transform the "save" username back to the original IAM "unsave" username. Fixes upstream issue #27

--- authorized_keys_command.sh
+++ authorized_keys_command.sh
@@ -4,12 +4,32 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
+# Assume a role before contacting AWS IAM to get users and keys.
+# This can be used if you define your users in one AWS account, while the EC2
+# instance you use this script runs in another.
+ASSUMEROLE=""
+
+if [[ ! -z "${ASSUMEROLE}" ]]
+then
+  STSCredentials=$(aws sts assume-role \
+    --role-arn "${ASSUMEROLE}" \
+    --role-session-name something \
+    --query '[Credentials.SessionToken,Credentials.AccessKeyId,Credentials.SecretAccessKey]' \
+    --output text)
+
+  AWS_ACCESS_KEY_ID=$(echo "${STSCredentials}" | awk '{print $2}')
+  AWS_SECRET_ACCESS_KEY=$(echo "${STSCredentials}" | awk '{print $3}')
+  AWS_SESSION_TOKEN=$(echo "${STSCredentials}" | awk '{print $1}')
+  AWS_SECURITY_TOKEN=$(echo "${STSCredentials}" | awk '{print $1}')
+  export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN AWS_SECURITY_TOKEN
+fi
+
 UnsaveUserName="$1"
 UnsaveUserName=${UnsaveUserName//".plus."/"+"}
 UnsaveUserName=${UnsaveUserName//".equal."/"="}
 UnsaveUserName=${UnsaveUserName//".comma."/","}
 UnsaveUserName=${UnsaveUserName//".at."/"@"}
 
-aws iam list-ssh-public-keys --user-name "$UnsaveUserName" --query "SSHPublicKeys[?Status == 'Active'].[SSHPublicKeyId]" --output text | while read KeyId; do
+aws iam list-ssh-public-keys --user-name "$UnsaveUserName" --query "SSHPublicKeys[?Status == 'Active'].[SSHPublicKeyId]" --output text | while read -r KeyId; do
   aws iam get-ssh-public-key --user-name "$UnsaveUserName" --ssh-public-key-id "$KeyId" --encoding SSH --query "SSHPublicKey.SSHPublicKeyBody" --output text
 done
