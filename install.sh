f2c832ad77e343e0e7b77a3b5014d405913d99f3
Michiel van Baak <michiel@vanbaak.eu>
2017-05-03 19:00:09 +0200

    Make useradd program and arguments configurable (#45)
    * Make both the useradd program and the arguments for useradd configurable. Closes #39
    
    * fix documentation

--- install.sh
+++ install.sh
@@ -2,22 +2,26 @@
 
 show_help() {
 cat << EOF
-Usage: ${0##*/} [-hv] [-a ARN] [-i GROUP,GROUP,...] [-l GROUP,GROUP,...] [-s GROUP]
+Usage: ${0##*/} [-hv] [-a ARN] [-i GROUP,GROUP,...] [-l GROUP,GROUP,...] [-s GROUP] [-p PROGRAM] [-u "ARGUMENTS"]
 Install import_users.sh and authorized_key_commands.
 
-    -h              display this help and exit
-    -v              verbose mode.
+    -h                 display this help and exit
+    -v                 verbose mode.
 
-    -a arn          Assume a role before contacting AWS IAM to get users and keys.
-                    This can be used if you define your users in one AWS account, while the EC2
-                    instance you use this script runs in another.
-    -i group,group  Which IAM groups have access to this instance
-                    Comma seperated list of IAM groups. Leave empty for all available IAM users
-    -l group,group  Give the users these local UNIX groups
-                    Comma seperated list
-    -s group        Specify an IAM group for users who should be given sudo privileges, or leave
-                    empty to not change sudo access, or give it the value '##ALL##' to have all
-                    users be given sudo rights.
+    -a arn             Assume a role before contacting AWS IAM to get users and keys.
+                       This can be used if you define your users in one AWS account, while the EC2
+                       instance you use this script runs in another.
+    -i group,group     Which IAM groups have access to this instance
+                       Comma seperated list of IAM groups. Leave empty for all available IAM users
+    -l group,group     Give the users these local UNIX groups
+                       Comma seperated list
+    -s group           Specify an IAM group for users who should be given sudo privileges, or leave
+                       empty to not change sudo access, or give it the value '##ALL##' to have all
+                       users be given sudo rights.
+    -p program         Specify your useradd program to use.
+                       Defaults to '/usr/sbin/useradd'
+    -u "useradd args"  Specify arguments to use with useradd.
+                       Defaults to '--create-home --shell /bin/bash'
 
 
 EOF
@@ -27,6 +31,8 @@ IAM_GROUPS=""
 SUDO_GROUP=""
 LOCAL_GROUPS=""
 ASSUME_ROLE=""
+USERADD_PROGRAM=""
+USERADD_ARGS=""
 
 while getopts :hva:i:l:s: opt
 do
@@ -49,6 +55,12 @@ do
             ;;
         a)
             ASSUME_ROLE="$OPTARG"
+            ;;
+        p)
+            USERADD_PROGRAM="$OPTARG"
+            ;;
+        u)
+            USERADD_ARGS="$OPTARG"
             ;;
         \?)
             echo "Invalid option: -$OPTARG" >&2
@@ -105,6 +117,16 @@ fi
 if [ "${ASSUME_ROLE}" != "" ]
 then
     echo "ASSUMEROLE=\"${ASSUME_ROLE}\"" >> /etc/aws-ec2-ssh.conf
+fi
+
+if [ "${USERADD_PROGRAM}" != "" ]
+then
+    echo "USERADD_PROGRAM=\"${USERADD_PROGRAM}\"" >> /etc/aws-ec2-ssh.conf
+fi
+
+if [ "${USERADD_ARGS}" != "" ]
+then
+    echo "USERADD_ARGS=\"${USERADD_ARGS}\"" >> /etc/aws-ec2-ssh.conf
 fi
 
 sed -i 's:#AuthorizedKeysCommand none:AuthorizedKeysCommand /opt/authorized_keys_command.sh:g' /etc/ssh/sshd_config
