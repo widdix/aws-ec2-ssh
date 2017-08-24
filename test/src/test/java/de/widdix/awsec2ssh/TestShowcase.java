30656443888484485d1c950aacfd8c263a563c48
Michael Wittig <post@michaelwittig.info>
2017-08-24 14:09:53 +0200

    added ubuntu test

--- test/src/test/java/de/widdix/awsec2ssh/TestShowcase.java
+++ test/src/test/java/de/widdix/awsec2ssh/TestShowcase.java
@@ -6,7 +6,30 @@ import org.junit.Test;
 public class TestShowcase extends ACloudFormationTest {
 
     @Test
-    public void test() throws Exception {
+    public void testUbuntu() throws Exception {
+        final String stackName = "showcase-" + this.random8String();
+        final String userName = "user-" + this.random8String();
+        try {
+            final User user = this.createUser(userName);
+            try {
+                this.createStack(stackName,
+                        "showcase.yaml",
+                        new Parameter().withParameterKey("VPC").withParameterValue(this.getDefaultVPC().getVpcId()),
+                        new Parameter().withParameterKey("Subnet").withParameterValue(this.getDefaultSubnets().get(0).getSubnetId()),
+                        new Parameter().withParameterKey("OS").withParameterValue("Ubuntu")
+                );
+                final String host = this.getStackOutputValue(stackName, "PublicName");
+                this.probeSSH(host, user);
+            } finally {
+                this.deleteStack(stackName);
+            }
+        } finally {
+            this.deleteUser(userName);
+        }
+    }
+
+    @Test
+    public void testDefaultAmazonLinux() throws Exception {
         final String stackName = "showcase-" + this.random8String();
         final String userName = "user-" + this.random8String();
         try {
