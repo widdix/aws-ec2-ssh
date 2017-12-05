22b21f1aa2788353161532f5d7f2a0fcae3cf163
KusabiSensei <KusabiSensei@users.noreply.github.com>
2017-12-05 11:07:44 -0500

    Add SELinux support to install script
    If we have the `getenforce` command available to us, and we find that
    it returns the value `Enforcing` (meaning SELinux will block our access
    to AWS when called by `sshd`), we enable the `nis_enabled` boolean
    persistently in SELinux to inform SELinux that it should expect outbound
    access from sshd and other login programs (like PAM) to remote servers
    to get user account information.
    
    This allows `sshd` to call the script to get the user's public key from IAM.
    
    I also cleaned up the `systemd` conditional in the script to use the
    simpler way to check if commands exist.

--- install.sh
+++ install.sh
@@ -148,17 +148,25 @@ chmod 0644 /etc/cron.d/import_users
 
 $IMPORT_USERS_SCRIPT_FILE
 
+# In order to support SELinux in Enforcing mode, we need to tell SELinux that it
+# should have the nis_enabled boolean turned on (so it should expect login services
+# like PAM and sshd to make calls to get public keys from a remote server)
+#
+# This is observed on CentOS 7 and RHEL 7
+
+if [[ `which getenforce` -eq "0" ]]; then
+  if [[ `getenforce | grep -q "Enforcing"` -eq "0" ]]; then
+    setsebool -P nis_enabled on
+  fi
+fi
+
 # Restart sshd using an appropriate method based on the currently running init daemon
 # Note that systemd can return "running" or "degraded" (If a systemd unit has failed)
 # This was observed on the RHEL 7.3 AMI, so it's added for completeness
 # systemd is also not standardized in the name of the ssh service, nor in the places
 # where the unit files are stored.
 
-# Capture the return code and use that to determine if we have the command available
-which systemctl > /dev/null 2>&1
-retval=$?
-
-if [[ "$retval" -eq "0" ]]; then
+if [[ `which systemctl` -eq "0" ]]; then
   if [[ (`systemctl is-system-running` =~ running) || (`systemctl is-system-running` =~ degraded) ]]; then
     if [ -f "/usr/lib/systemd/system/sshd.service" ] || [ -f "/lib/systemd/system/sshd.service" ]; then
       systemctl restart sshd.service
