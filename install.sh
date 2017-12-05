0034e270e5a0d601298b143671ba9d0b0bff5c98
KusabiSensei <KusabiSensei@users.noreply.github.com>
2017-12-05 15:55:33 -0500

    Allow which(1) to exit and the script to continue
    We use `which(1)` to determine if a command is available to us. According
    to the man page, `which(1)` will return a return code `1` if the command is
    not found. Since the script is run with `-e` as an option in the shebang,
    this causes the script to exit prematurely.
    
    I've wrapped the `which(1)` calls in `set +e/set -e` wrappers so that the
    script continues gracefully.

--- install.sh
+++ install.sh
@@ -155,11 +155,17 @@ $IMPORT_USERS_SCRIPT_FILE
 # This is observed on CentOS 7 and RHEL 7
 
 # Capture the return code and use that to determine if we have the command available
+# We also want to continue on non-zero exit codes, because this determines if we have
+# the command available to us
+set +e
 which getenforce > /dev/null 2>&1
 retval=$?
+set -e
 
 if [[ "$retval" -eq "0" ]]; then
-  if [[ `getenforce | grep -q "Enforcing"` -eq "0" ]]; then
+  selinuxenabled
+  retval=$?
+  if [[ "$retval" -eq "0" ]]; then
     setsebool -P nis_enabled on
   fi
 fi
@@ -172,8 +178,12 @@ fi
 # where the unit files are stored.
 
 # Capture the return code and use that to determine if we have the command available
+# We also want to continue on non-zero exit codes, because this determines if we have
+# the command available to us
+set +e
 which systemctl > /dev/null 2>&1
 retval=$?
+set -e
 
 if [[ "$retval" -eq "0" ]]; then
   if [[ (`systemctl is-system-running` =~ running) || (`systemctl is-system-running` =~ degraded) ]]; then
