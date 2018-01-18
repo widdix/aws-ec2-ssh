7a4e4eb7b54840ec6229e28070d886b889024a0d
Michael Wittig <post@michaelwittig.info>
2018-01-18 15:07:13 +0100

    refs #115

---
+++ test/src/test/java/de/widdix/awsec2ssh/TestShowcaseRPM.java
@@ -0,0 +1,145 @@
+package de.widdix.awsec2ssh;
+
+import com.amazonaws.services.cloudformation.model.Parameter;
+import org.junit.Test;
+
+public class TestShowcase extends ACloudFormationTest {
+
+    @Test
+    public void testCentOS() throws Exception {
+        final String stackName = "showcase-rpm-" + this.random8String();
+        final String userName = "user-" + this.random8String();
+        try {
+            final User user = this.createUser(userName);
+            try {
+                this.createStack(stackName,
+                        "showcase-rpm.yaml",
+                        new Parameter().withParameterKey("VPC").withParameterValue(this.getDefaultVPC().getVpcId()),
+                        new Parameter().withParameterKey("Subnet").withParameterValue(this.getDefaultSubnets().get(0).getSubnetId()),
+                        new Parameter().withParameterKey("OS").withParameterValue("CentOS")
+                );
+                final String host = this.getStackOutputValue(stackName, "PublicName");
+                this.probeSSH(host, user);
+            } finally {
+                this.deleteStack(stackName);
+            }
+        } finally {
+            this.deleteUser(userName);
+        }
+    }
+
+    @Test
+    public void testRHEL() throws Exception {
+        final String stackName = "showcase-rpm-" + this.random8String();
+        final String userName = "user-" + this.random8String();
+        try {
+            final User user = this.createUser(userName);
+            try {
+                this.createStack(stackName,
+                        "showcase-rpm.yaml",
+                        new Parameter().withParameterKey("VPC").withParameterValue(this.getDefaultVPC().getVpcId()),
+                        new Parameter().withParameterKey("Subnet").withParameterValue(this.getDefaultSubnets().get(0).getSubnetId()),
+                        new Parameter().withParameterKey("OS").withParameterValue("RHEL")
+                );
+                final String host = this.getStackOutputValue(stackName, "PublicName");
+                this.probeSSH(host, user);
+            } finally {
+                this.deleteStack(stackName);
+            }
+        } finally {
+            this.deleteUser(userName);
+        }
+    }
+
+    @Test
+    public void testSUSELinuxEnterpriseServer() throws Exception {
+        final String stackName = "showcase-rpm-" + this.random8String();
+        final String userName = "user-" + this.random8String();
+        try {
+            final User user = this.createUser(userName);
+            try {
+                this.createStack(stackName,
+                        "showcase-rpm.yaml",
+                        new Parameter().withParameterKey("VPC").withParameterValue(this.getDefaultVPC().getVpcId()),
+                        new Parameter().withParameterKey("Subnet").withParameterValue(this.getDefaultSubnets().get(0).getSubnetId()),
+                        new Parameter().withParameterKey("OS").withParameterValue("SUSELinuxEnterpriseServer")
+                );
+                final String host = this.getStackOutputValue(stackName, "PublicName");
+                this.probeSSH(host, user);
+            } finally {
+                this.deleteStack(stackName);
+            }
+        } finally {
+            this.deleteUser(userName);
+        }
+    }
+
+    @Test
+    public void testUbuntu() throws Exception {
+        final String stackName = "showcase-rpm-" + this.random8String();
+        final String userName = "user-" + this.random8String();
+        try {
+            final User user = this.createUser(userName);
+            try {
+                this.createStack(stackName,
+                        "showcase-rpm.yaml",
+                        new Parameter().withParameterKey("VPC").withParameterValue(this.getDefaultVPC().getVpcId()),
+                        new Parameter().withParameterKey("Subnet").withParameterValue(this.getDefaultSubnets().get(0).getSubnetId()),
+                        new Parameter().withParameterKey("OS").withParameterValue("Ubuntu")
+                );
+                final String host = this.getStackOutputValue(stackName, "PublicName");
+                this.probeSSH(host, user);
+            } finally {
+                this.deleteStack(stackName);
+            }
+        } finally {
+            this.deleteUser(userName);
+        }
+    }
+
+    @Test
+    public void testAmazonLinux2() throws Exception {
+        final String stackName = "showcase-rpm-" + this.random8String();
+        final String userName = "user-" + this.random8String();
+        try {
+            final User user = this.createUser(userName);
+            try {
+                this.createStack(stackName,
+                        "showcase-rpm.yaml",
+                        new Parameter().withParameterKey("VPC").withParameterValue(this.getDefaultVPC().getVpcId()),
+                        new Parameter().withParameterKey("Subnet").withParameterValue(this.getDefaultSubnets().get(0).getSubnetId()),
+                        new Parameter().withParameterKey("OS").withParameterValue("AmazonLinux2")
+                );
+                final String host = this.getStackOutputValue(stackName, "PublicName");
+                this.probeSSH(host, user);
+            } finally {
+                this.deleteStack(stackName);
+            }
+        } finally {
+            this.deleteUser(userName);
+        }
+    }
+
+    @Test
+    public void testDefaultAmazonLinux() throws Exception {
+        final String stackName = "showcase-rpm-" + this.random8String();
+        final String userName = "user-" + this.random8String();
+        try {
+            final User user = this.createUser(userName);
+            try {
+                this.createStack(stackName,
+                        "showcase-rpm.yaml",
+                        new Parameter().withParameterKey("VPC").withParameterValue(this.getDefaultVPC().getVpcId()),
+                        new Parameter().withParameterKey("Subnet").withParameterValue(this.getDefaultSubnets().get(0).getSubnetId())
+                );
+                final String host = this.getStackOutputValue(stackName, "PublicName");
+                this.probeSSH(host, user);
+            } finally {
+                this.deleteStack(stackName);
+            }
+        } finally {
+            this.deleteUser(userName);
+        }
+    }
+
+}
