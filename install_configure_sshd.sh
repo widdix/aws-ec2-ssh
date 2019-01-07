a2b275e8d14a62a67cca69a66842a643dc2e9f70
Michael Wittig <michael@widdix.de>
2019-01-07 15:35:50 +0100

    fix IAM SSH access

--- install_configure_sshd.sh
+++ install_configure_sshd.sh
@@ -1,17 +1,9 @@
 #!/bin/bash -e
 
-if grep -q '#AuthorizedKeysCommand none' "$SSHD_CONFIG_FILE"; then
-  sed -i "s:#AuthorizedKeysCommand none:AuthorizedKeysCommand ${AUTHORIZED_KEYS_COMMAND_FILE}:g" "$SSHD_CONFIG_FILE"
-else
-  if ! grep -q "AuthorizedKeysCommand ${AUTHORIZED_KEYS_COMMAND_FILE}" "$SSHD_CONFIG_FILE"; then
-    echo "AuthorizedKeysCommand ${AUTHORIZED_KEYS_COMMAND_FILE}" >> "$SSHD_CONFIG_FILE"
-  fi
+if ! grep -q '^AuthorizedKeysCommand /opt/authorized_keys_command.sh' /etc/ssh/sshd_config; then
+	sed -e '/AuthorizedKeysCommand / s/^#*/#/' -i /etc/ssh/sshd_config; echo 'AuthorizedKeysCommand /opt/authorized_keys_command.sh' >> /etc/ssh/sshd_config
 fi
 
-if grep -q '#AuthorizedKeysCommandUser nobody' "$SSHD_CONFIG_FILE"; then
-  sed -i "s:#AuthorizedKeysCommandUser nobody:AuthorizedKeysCommandUser nobody:g" "$SSHD_CONFIG_FILE"
-else
-  if ! grep -q 'AuthorizedKeysCommandUser nobody' "$SSHD_CONFIG_FILE"; then
-    echo "AuthorizedKeysCommandUser nobody" >> "$SSHD_CONFIG_FILE"
-  fi
+if ! grep -q '^AuthorizedKeysCommandUser nobody' /etc/ssh/sshd_config; then
+	sed -e '/AuthorizedKeysCommandUser / s/^#*/#/' -i /etc/ssh/sshd_config; echo 'AuthorizedKeysCommandUser nobody' >> /etc/ssh/sshd_config
 fi
