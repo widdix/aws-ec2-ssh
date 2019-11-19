970de9f9c405de2d534e5ab906db17d9cb765359
maartenkamoen <maartenkamoen@users.noreply.github.com>
2019-11-19 11:15:18 +0100

    Update import_users.sh
    Fixed order - get ec2 tags before switching to specific role for getting users

--- import_users.sh
+++ import_users.sh
@@ -238,9 +238,6 @@ function sync_accounts() {
     # Check if local marker group exists, if not, create it
     /usr/bin/getent group "${LOCAL_MARKER_GROUP}" >/dev/null 2>&1 || /usr/sbin/groupadd "${LOCAL_MARKER_GROUP}"
 
-    # setup the aws credentials if needed
-    setup_aws_credentials
-
     # declare and set some variables
     local iam_users
     local sudo_users
@@ -252,6 +249,9 @@ function sync_accounts() {
     # init group and sudoers from tags
     get_iam_groups_from_tag
     get_sudoers_groups_from_tag
+
+    # setup the aws credentials if needed
+    setup_aws_credentials
 
     iam_users=$(get_clean_iam_users | sort | uniq)
     sudo_users=$(get_clean_sudoers_users | sort | uniq)
