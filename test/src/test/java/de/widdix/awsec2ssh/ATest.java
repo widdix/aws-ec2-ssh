2649009e1e01c7c5b9fe1f3f295ac2263bbf5a44
Michael Wittig <michael@widdix.de>
2021-02-08 09:41:06 +0100

    improve test suite

--- test/src/test/java/de/widdix/awsec2ssh/ATest.java
+++ test/src/test/java/de/widdix/awsec2ssh/ATest.java
@@ -1,25 +1,30 @@
 package de.widdix.awsec2ssh;
 
+import com.amazonaws.services.ec2.model.KeyPair;
 import com.evanlennick.retry4j.CallExecutor;
 import com.evanlennick.retry4j.CallResults;
 import com.evanlennick.retry4j.RetryConfig;
 import com.evanlennick.retry4j.RetryConfigBuilder;
 import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import org.junit.Assert;
 
 import java.time.temporal.ChronoUnit;
 import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public abstract class ATest {
 
-    protected final <T> T retry(Callable<T> callable) {
+    protected final <T> T retry(final Context context, final Callable<T> callable) {
+        final AtomicInteger t = new AtomicInteger(0);
         final Callable<T> wrapper = () -> {
             try {
                 return callable.call();
             } catch (final Exception e) {
-                System.out.println("retry[] exception: " + e.getMessage());
-                e.printStackTrace();
+                System.out.println("retry[" + t.incrementAndGet() + "] exception: " + e.getMessage());
+                e.printStackTrace(System.out);
+                System.out.println();
                 throw e;
             }
         };
@@ -29,14 +34,20 @@ public abstract class ATest {
                 .withDelayBetweenTries(10, ChronoUnit.SECONDS)
                 .withFixedBackoff()
                 .build();
-        final CallResults<Object> results = new CallExecutor(config).execute(wrapper);
-        return (T) results.getResult();
+        try {
+            final CallResults<Object> results = new CallExecutor(config).execute(wrapper);
+            return (T) results.getResult();
+        } catch (final RuntimeException e) {
+            context.reportFailure();
+            throw e;
+        }
     }
 
     public static final class User {
         public final String userName;
         public final byte[] sshPrivateKeyBlob;
         public final String sshPublicKeyId;
+
         public User(final String userName, final byte[] sshPrivateKeyBlob, final String sshPublicKeyId) {
             super();
             this.userName = userName;
@@ -45,7 +56,7 @@ public abstract class ATest {
         }
     }
 
-    protected final void probeSSH(final String host, final User user) {
+    protected final void probeSSH(final Context context, final String host, final User user) {
         final Callable<Boolean> callable = () -> {
             final JSch jsch = new JSch();
             final Session session = jsch.getSession(user.userName, host);
@@ -55,7 +66,30 @@ public abstract class ATest {
             session.disconnect();
             return true;
         };
-        Assert.assertTrue(this.retry(callable));
+        Assert.assertTrue("successful SSH connection", this.retry(context, callable));
+    }
+
+    protected final void probeSSH(final Context context, final String host, final KeyPair key) {
+        final Callable<Boolean> callable = () -> {
+            final JSch jsch = new JSch();
+            final Session session = jsch.getSession("ec2-user", host);
+            jsch.addIdentity(key.getKeyName(), key.getKeyMaterial().getBytes(), null, null);
+            jsch.setConfig("StrictHostKeyChecking", "no"); // for testing this should be fine. adding the host key seems to be only possible via a file which is not very useful here
+            session.connect(10000);
+            session.disconnect();
+            return true;
+        };
+        Assert.assertTrue("successful SSH connection", this.retry(context, callable));
+    }
+
+    protected final Session tunnelSSH(final String host, final KeyPair key, final Integer localPort, final String remoteHost, final Integer remotePort) throws JSchException {
+        final JSch jsch = new JSch();
+        final Session session = jsch.getSession("ec2-user", host);
+        jsch.addIdentity(key.getKeyName(), key.getKeyMaterial().getBytes(), null, null);
+        jsch.setConfig("StrictHostKeyChecking", "no"); // for testing this should be fine. adding the host key seems to be only possible via a file which is not very useful here
+        session.setPortForwardingL(localPort, remoteHost, remotePort);
+        session.connect(10000);
+        return session;
     }
 
 }
